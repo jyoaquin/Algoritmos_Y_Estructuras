76d3ce3f825cc095693cc2bfa99e4e79ffdfc705
jyoaquin <jyoaquin@hotmail.com>
2024-10-25 20:35:00 -0300

    ACRUALIZACION DE POO

--- Programacion_POO.py
+++ Programacion_POO.py
@@ -1,21 +1,42 @@
 
+# class Persona():
+#     caminando = False
+#     nombre = ""
+#     apellido = ""
+
+#     def caminar(self):
+#         self.caminando = True
+#         print(f"Soy {self.nombre} {self.apellido} y estoy caminando!")
+    
+#     def detener(self):
+#         self.caminando = False
+#         print(f"Soy {self.nombre} {self.apellido} y estoy detenido")
+
+
+# juan = Persona()
+# juan.nombre = "Juan"
+# juan.apellido = "Perez"
+# juan.caminar()
+# juan.detener()
+
+
 class Persona():
     caminando = False
     nombre = ""
     apellido = ""
 
-    def caminar(self, nombre, apellido):
+    def caminar(self):
         self.caminando = True
-        print(f"Soy {nombre} {apellido} y estoy caminando!")
+        print(f"Soy {self.nombre} {self.apellido} y estoy caminando!")
     
-    def detener(self, nombre, apellido):
+    def detener(self):
         self.caminando = False
-        print(f"Soy {nombre} {apellido} y estoy detenido")
+        print(f"Soy {self.nombre} {self.apellido} y estoy detenido")
 
 
 juan = Persona()
-juan.nombre = "Juan"
-juan.apellido = "Perez"
-juan.detener(juan.nombre, juan.apellido)
-juan.caminar(juan.nombre, juan.apellido)  
+juan.nombre = input("Ingrese el nombre: ")[::-1]
+juan.apellido = input("Ingrese el apellido: ")[::-1]
+juan.caminar()
+juan.detener()
 
